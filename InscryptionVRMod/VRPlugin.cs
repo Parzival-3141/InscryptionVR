72b186110547c9ce1501184b1f23ac7a77c4c549
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2022-01-14 20:46:46 -0500

    Initial Commit

---
+++ InscryptionVRMod/VRPlugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+
+namespace InscryptionVR
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "parzival.inscryption.vrmod";
+        public const string PLUGIN_NAME = "InscryptionVRMod";
+        public const string PLUGIN_VERSION = "0.0.1";
+    }
+
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Inscryption.exe")]
+    public class VRPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
