b3cc95e4188f8666b33378798f1ef8f9f25c734a
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2022-01-16 04:22:28 -0500

    Camera fixes

---
+++ InscryptionVRMod/Modules/CameraFixes.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using DiskCardGame;
+using MonoMod.Cil;
+
+namespace InscryptionVRMod.Modules
+{
+    public static class CameraFixes
+    {
+        [HarmonyILManipulator]
+        [HarmonyPatch(typeof(PixelCamera), "LateUpdate")]
+        private static void FixPixelationIL(ILContext il)
+        {
+            ILCursor c = new(il);
+            c.GotoNext(x => x.MatchLdcR4(100f));
+            c.Next.Operand = 10f;
+        }
+    }
+}
